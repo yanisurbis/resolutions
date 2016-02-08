07de28a9dded3653d36d8f8760eb14e92c253722
Yanis <yanis.urbis@gmail.com>
2016-02-08 23:57:47 +0300

    Simple Blaze iteration and template helper for body tag.

--- resolutions.js
+++ resolutions.js
@@ -1,23 +1,14 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+  // template helper allows you to use information on the page
+  Template.body.helpers({
+    resolutions: [
+      { title: 'Hello Resolution #1' }
+    ]
+  })
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
-  });
+  })
 }
