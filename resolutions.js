c1b7bb5270f772d3c6292e74f761e96003a9e4af
Yanis <yanis.urbis@gmail.com>
2016-02-11 13:04:57 +0300

    Secure issues

--- resolutions.js
+++ resolutions.js
@@ -94,11 +94,23 @@ Meteor.methods({
     });
   },
   updateResolution: function(id, checked) {
+    var res = Resolutions.findOne(id);
+
+    if(res.owner !== Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+
     Resolutions.update(id, {$set :{
       checked: checked
     }});
   },
   deleteResolution: function(id) {
+    var res = Resolutions.findOne(id);
+
+    if(res.owner !== Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+
     Resolutions.remove(id);
   },
   setPrivate: function(id, private) {
